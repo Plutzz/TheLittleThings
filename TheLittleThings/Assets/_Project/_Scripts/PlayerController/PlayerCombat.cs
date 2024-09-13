cb57f2bc85293f60e98d0f3089257bd0a5414bd5
Isaiah <157159952+GrimMeteor@users.noreply.github.com>
2024-09-13 15:07:10 -0600

    Attack update(Isaiah)

---
+++ TheLittleThings/Assets/_Project/_Scripts/PlayerController/PlayerCombat.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+{
+
+    public Animator animator;
+    public Transform attackPoint;
+    public float attackRange = 0.5f;
+    public LayerMask enemyLayers;
+   
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Attack();
+        }
+    }
+    void Attack()
+    {
+        animator.Play("Attack1");
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+        foreach(Collider2D enemy in hitEnemies)
+        {
+            Debug.Log("We hit " + enemy.name);
+        }
+    }
+}
