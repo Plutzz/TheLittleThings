339417ea5ff80d936992348607e94a83e148ccac
KALE <114507504+kaleyvuong@users.noreply.github.com>
2024-09-15 20:55:02 -0700

    Bow Weapon
    Bow rotates around character
    Projectile shoots following mouse
    Projectile disappears upon collision/after couple secs

---
+++ TheLittleThings/Assets/_Project/_Scripts/PlayerController/PlayerRanged.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEditor.Tilemaps;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UIElements;
+
+
+public class PlayerRanged : MonoBehaviour
+{
+    public Camera cam;
+    public GameObject bullet;
+    public GameObject spawn;
+    public Transform parent;
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotate();
+        if (Input.GetKeyDown(KeyCode.Mouse0)) {
+            shoot();
+        }
+    }
+
+    void rotate () {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = 10;
+        mousePos = cam.ScreenToWorldPoint(mousePos);
+
+        Vector2 dir = new Vector3(mousePos.x - transform.position.x, mousePos.y - transform.position.y) * parent.localScale.x;
+        transform.right = dir;
+    }
+
+    void shoot() {
+        GameObject projectile = (GameObject)Instantiate(bullet, spawn.transform.position, Quaternion.identity);
+        projectile.transform.right = transform.right * parent.localScale.x;
+
+    }
+
+}
