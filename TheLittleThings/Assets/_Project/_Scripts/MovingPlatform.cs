777eca1f4a150bf4e3b4f75990fdb56f23d0f458
Ahmad Sawalha <ahmadsawalha123456@gmail.com>
2024-09-22 21:57:13 -0700

    Fixed MovingPlatform

--- TheLittleThings/Assets/_Project/_Scripts/MovingPlatform.cs
+++ TheLittleThings/Assets/_Project/_Scripts/MovingPlatform.cs
@@ -2,40 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class MovingPlatform : MonoBehaviour
 {
     [SerializeField] private Transform pos1, pos2;
-    [SerializeField] private int Speed;
-    Vector2 targetPos;
+    [SerializeField] private float Speed;
+    private Vector2 targetPos;
+    private Vector2 previousPosition;
+    private Vector2 platformVelocity;
+    [SerializeField] private float forceMultiplier;
 
     // Start is called before the first frame update
     void Start()
     {
         targetPos = pos2.position;
+        previousPosition = transform.position; 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Vector2.Distance(transform.position, pos1.position) < .1f) targetPos = pos2.position;
-        if(Vector2.Distance(transform.position, pos2.position) < .1f) targetPos = pos1.position;
+        if(Vector2.Distance(transform.position, pos1.position) < .1f)
+            targetPos = pos2.position;
+        if(Vector2.Distance(transform.position, pos2.position) < .1f)
+            targetPos = pos1.position;
 
         transform.position = Vector2.MoveTowards(transform.position, targetPos, Speed * Time.deltaTime);
+
+        platformVelocity = ((Vector2)transform.position - previousPosition) / Time.deltaTime;
+        previousPosition = transform.position;
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionStay2D(Collision2D collision)
     {
-        if(collision.collider.CompareTag("Player"))
+        if (collision.collider.CompareTag("Player"))
         {
-            collision.transform.SetParent(this.transform);
-        }
-    }
-
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        if(collision.collider.CompareTag("Player"))
-        {
-            collision.transform.SetParent(null);
+            Rigidbody2D playerRb = collision.transform.GetComponent<Rigidbody2D>();
+            if (playerRb != null)
+            {
+                playerRb.AddForce(new Vector2(platformVelocity.x * forceMultiplier, 0), ForceMode2D.Force);
+            }
         }
     }
 }
