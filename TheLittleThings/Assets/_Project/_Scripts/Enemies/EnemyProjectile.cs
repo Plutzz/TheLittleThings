ddc5f0cdaa61181ea4db2bc61d86c263d3a656ab
KALE <114507504+kaleyvuong@users.noreply.github.com>
2024-10-06 21:30:09 -0700

    PEWPEWPWEPWEPEPWPEWPEWPEPWPEW
    it shoots forward every couple of seconds

--- TheLittleThings/Assets/_Project/_Scripts/Enemies/EnemyProjectile.cs
+++ TheLittleThings/Assets/_Project/_Scripts/Enemies/EnemyProjectile.cs
@@ -2,21 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerProjectile : MonoBehaviour
+public class EnemyProjectile : MonoBehaviour
 {
     float Hspeed = 28;
     public GameObject hitEffect;
+    public GameObject Player;
+    [SerializeField] private Rigidbody m_RigidBody;
 
-    // Update is called once per frame
+    void Awake () {
+        m_RigidBody = GetComponent<Rigidbody>();
+    }
     void Update()
     {
-        transform.Translate(Vector2.right * Hspeed * Time.deltaTime);
+        m_RigidBody.AddForce(m_RigidBody.transform.forward * Hspeed);
         Destroy(gameObject, 1f);
-
     }
-
     void OnCollisionEnter(Collision collision) {
         GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+        Physics2D.IgnoreCollision(Player.GetComponent<Collider2D>(), GetComponent<Collider2D>());
         Destroy(effect, 5f);
         Destroy(gameObject);
     }
---
+++ TheLittleThings/Assets/_Project/_Scripts/Enemies/EnemyRanged.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEditor.Tilemaps;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UIElements;
+
+
+public class EnemyRanged : MonoBehaviour
+{
+    public GameObject spike;
+    public Transform spawn;
+    public float time = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (time > 2f) {
+            shoot();
+            time = 0f;
+        }
+    }
+    void shoot() {
+        GameObject projectile = (GameObject)Instantiate(spike, spawn.transform.position, spawn.rotation);
+    }
+
+}
