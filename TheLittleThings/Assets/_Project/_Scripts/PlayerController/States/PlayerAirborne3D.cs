8bc11627224f249186a4b25ff12f808ad5832a6e
Plutzz <benjiongco@gmail.com>
2024-10-09 20:17:29 -0700

    Change airborne state

--- TheLittleThings/Assets/_Project/_Scripts/PlayerController/States/PlayerAirborne3D.cs
+++ TheLittleThings/Assets/_Project/_Scripts/PlayerController/States/PlayerAirborne3D.cs
@@ -19,7 +19,6 @@ public class PlayerAirborne3D : State
     public override void DoFixedUpdateState()
     {
         rb.velocity = (new Vector3(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -stats.FallSpeedLimit, stats.FallSpeedLimit), rb.velocity.z));
-
         //checks if player wants to move in the opposite direction of velocity
         // NOTE: feels a little touchy, maybe instead of instantly setting velo to 0 we make it so that
         // if the player isn't holding the direction they are moving in we add a decel force
