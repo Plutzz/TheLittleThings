c485c9e1a96c4b922e30c117eede58f62f86b4b4
Plutzz <benjiongco@gmail.com>
2024-10-09 20:08:41 -0700

    Change Airborne script

--- TheLittleThings/Assets/_Project/_Scripts/PlayerController/States/PlayerAirborne3D.cs
+++ TheLittleThings/Assets/_Project/_Scripts/PlayerController/States/PlayerAirborne3D.cs
@@ -18,32 +18,13 @@ public class PlayerAirborne3D : State
 
     public override void DoFixedUpdateState()
     {
-        rb.velocity = (new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -stats.FallSpeedLimit, stats.FallSpeedLimit)));
+        rb.velocity = (new Vector3(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -stats.FallSpeedLimit, stats.FallSpeedLimit), rb.velocity.z));
 
         //checks if player wants to move in the opposite direction of velocity
         // NOTE: feels a little touchy, maybe instead of instantly setting velo to 0 we make it so that
         // if the player isn't holding the direction they are moving in we add a decel force
         // (so if you aren't pressing an input then the player will start heading towards 0 velocity)
-        if (Mathf.Abs(new Vector3(rb.velocity.x, 0, rb.velocity.y).magnitude) < stats.MaxSpeed)
-        {
-            rb.AddForce((orientation.forward * playerInput.yInput + orientation.right * playerInput.xInput).normalized * (stats.Acceleration));
-        }
 
-        //if velocity is less than maxspeed
-        //if (Mathf.Abs(rb.velocity.x) < stats.MaxSpeed)
-        //{
-        //    rb.AddForce(Vector2.right * playerInput.xInput * stats.Acceleration);
-        //}
-        //else //if at max speed, set velocity to max speed for consistent movement
-        //{
-        //    if (rb.velocity.x < -stats.MaxSpeed)
-        //    {
-        //        rb.velocity = new Vector2(Mathf.Clamp(-stats.MaxSpeed, -stats.MaxSpeed, 0), rb.velocity.y);
-        //    }
-        //    else if (rb.velocity.x > stats.MaxSpeed)
-        //    {
-        //        rb.velocity = new Vector2(Mathf.Clamp(stats.MaxSpeed, 0, stats.MaxSpeed), rb.velocity.y);
-        //    }
-        //}
+        rb.AddForce((orientation.forward * playerInput.yInput + orientation.right * playerInput.xInput).normalized * (stats.Acceleration));
     }
 }
