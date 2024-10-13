026a9e82b0f8dda727230940731c41a6badb3801
KALE <114507504+kaleyvuong@users.noreply.github.com>
2024-10-13 16:09:45 -0700

    added particle system to spike prefab (no script just component)

--- TheLittleThings/Assets/_Project/_Scripts/Enemies/EnemyProjectile.cs
+++ TheLittleThings/Assets/_Project/_Scripts/Enemies/EnemyProjectile.cs
@@ -9,6 +9,7 @@ public class EnemyProjectile : MonoBehaviour
     [SerializeField] private Rigidbody rb;
     [SerializeField] private float m_Speed;
     [SerializeField] private int damage;
+    [SerializeField] private float duration;
 
     void Awake () {
         rb = GetComponent<Rigidbody>();
@@ -16,17 +17,12 @@ public class EnemyProjectile : MonoBehaviour
     void Update()
     {
         rb.AddForce(rb.transform.forward * m_Speed);
-        // rb.velocity = new Vector3(m_Speed * Mathf.Sign(rb.transform.forward.x), rb.velocity.y, rb.velocity.z);
-        Destroy(gameObject, 1f);
+        Destroy(gameObject, duration);
     }
     void OnCollisionEnter(Collision collision) {
-        
         if (collision.gameObject == Enemy) return;
-        GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);  
         if (collision.gameObject.CompareTag("Player")) {
             collision.gameObject.GetComponent<PlayerHealth>().TakeDamage(damage);
         } 
-            Destroy(effect, 5f);
-            Destroy(gameObject);
     }
 }
